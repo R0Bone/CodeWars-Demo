12000d47dad8bfec9928b6f0559f01fe848fc4ca
Arreola <ra3020343@gmail.com>
2022-05-16 19:46:20 -0700

    codewars solutions

---
+++ js/solution001.js
@@ -0,0 +1,12 @@
+function removeChar(string){
+    //asign new variable string 
+    let result = string.slice(1,-1)
+    console.log(result)
+   
+   };
+   
+
+   
+   removeChar("hippo")/// ipp 
+   // removeChar("hamburger")// amburge
+   // removeChar("buster") // uste
---
+++ js/solution002.js
@@ -0,0 +1,20 @@
+//Create a function that takes an integer as an argument and returns
+//"Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+//conditional deciding whether number is "Even" or "Odd"
+    if(number % 2===0){
+        console.log("Even")
+    }else{
+        console.log("Odd")
+    }
+
+}
+  
+
+even_or_odd(6)//"Even"
+
+//even_or_odd(3)///"Odd"
+
+//return "Even" for even numbers
+//return "Odd" for odd numbers 
