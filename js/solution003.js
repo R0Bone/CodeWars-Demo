01ca8e32fd415bca61650dc01363d0ad6f93af98
Arreola <ra3020343@gmail.com>
2022-05-17 21:15:42 -0700

    solution003 completed

---
+++ js/solution003.js
@@ -0,0 +1,14 @@
+//Very simple, given an integer or a floating-point number, find its opposite.
+
+// parameters: decimals, integers 
+// return: opposite integer 
+
+function opposite(number) {
+    let oppNum = number * -1
+     console.log(oppNum)
+  }
+
+
+  opposite(1)///-1
+  opposite(14)///-14
+  opposite(-3)///3
