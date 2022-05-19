4940ba9be095001b897e8b76dc0ab36f6cb354d5
Arreola <ra3020343@gmail.com>
2022-05-19 16:01:00 -0700

    solution005 completed

---
+++ js/solution005.js
@@ -0,0 +1,18 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+function boolToWord( bool ){
+    //create a conditonal 
+    if(bool===true){
+        return "Yes"
+      }else{ 
+          return "No"
+      }
+    //return "Yes" or "No"
+    }
+boolToWord(true)
+boolToWord(8)
+boolToWord(false)
+//return "Yes" for true or "No" for false 
+  //example: boo1ToWord(true)///Yes
+  //example: boo1ToWord(8)///No
+  //example: boo1ToWord(false)///No
+  
