c663c92a0282f991c21738ab54c0a3f009dd7607
Arreola <ra3020343@gmail.com>
2022-06-16 19:29:54 -0700

    solution028 completed

--- js/solution027.js
+++ js/solution027.js
@@ -10,7 +10,7 @@ function find_average(array) {
       newArray += array[i] / array.length
    
     }
-    return newArray // return average
+    return newArray // return average of the numbers in a given list
   }
 
   console.log(find_average([3,4,5,6]))// => 4.5
