8d94de20acd27896d1e90735f211af730dc85392
Arreola <ra3020343@gmail.com>
2022-07-04 22:24:33 -0700

    solution042 completed

---
+++ js/solution042.js
@@ -0,0 +1,10 @@
+// Sum Numbers
+//Write a function that takes an array of numbers and returns the sum of the numbers.
+//The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+function sum (numbers) {
+   return numbers.reduce((a,b) => a + b, 0)   
+}
+
+console.log(sum([3,4,-1,-5,2]))//3
+console.log(sum([]))//0
+console.log(sum([1.5,2,3,3.5]))//10
