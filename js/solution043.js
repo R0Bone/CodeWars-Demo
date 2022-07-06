6474bfe090cf42e9dd136bc1843854fc41d7f591
Arreola <ra3020343@gmail.com>
2022-07-05 19:25:14 -0700

    solution043 completed

---
+++ js/solution043.js
@@ -0,0 +1,18 @@
+/*You are going to be given a word. Your job is to return the middle character of the word.
+ If the word's length is odd, ****return the middle character. If the word's length is even,
+ **** return the middle 2 characters.*/
+
+function getMiddle(str) { // input will be a string
+    let length = str.length
+    let middle = Math.floor(length / 2)
+    
+    if(length % 2 === 0){
+      return str[middle - 1] + str[middle]
+    }else{
+      return str[middle]
+      
+    }
+  }
+
+  console.log(getMiddle('super'))//'p'
+  console.log(getMiddle('winner')) // 'nn'
