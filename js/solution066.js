7382f37e8a5c548408349e3d313103d9c9b614b8
Arreola <ra3020343@gmail.com>
2022-08-03 23:02:20 -0700

    solution067 completed

--- js/solution066.js
+++ js/solution066.js
@@ -1,10 +1,9 @@
-function removeRotten(bagOfFruits){
-    
-    return bagOfFruits ? bagOfFruits.map(fruit =>fruit.replace('rotten','').toLowerCase()) : []
+function removeRotten(bagOfFruits) {
+  return bagOfFruits
+    ? bagOfFruits.map((fruit) => fruit.replace("rotten", "").toLowerCase())
+    : [];
+}
 
-    
-  }
-
-  console.log(removeRotten(['apple','peach','rottenBanana','pear']))// ['apple','peach',banana','pear']
-  console.log(removeRotten([]))// 
-  console.log(removeRotten())// 
+console.log(removeRotten(["apple", "peach", "rottenBanana", "pear"])); // ['apple','peach',banana','pear']
+console.log(removeRotten([])); //
+console.log(removeRotten()); //
---
+++ js/solution067.js
@@ -0,0 +1,20 @@
+/*function firstNonConsecutive (arr) {
+  let num = ''
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i] < i + 1 ){
+            num += arr[i]
+        }
+    }
+}
+*/
+function firstNonConsecutive (arr) {
+    for (let i = 0; i<arr.length - 1; i++) {
+      if(arr[i]+1 !== arr[i+1]) {
+       return arr[i+1]
+     } 
+       
+     }
+      return null
+    }
+
+console.log(firstNonConsecutive([1,2,3,4,6,7,8,9]))
