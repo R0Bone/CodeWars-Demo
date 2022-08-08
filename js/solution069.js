a296809b8761730100c1a35d4a054c9b8132dd41
Arreola <ra3020343@gmail.com>
2022-08-07 22:07:53 -0700

    solution069 completed

---
+++ js/solution069.js
@@ -0,0 +1,16 @@
+function DNAtoRNA(dna) {
+  let result = ''  
+ //let rna = dna.split('')
+ for(let i = 0;i< dna.length;i++){
+     if(dna[i] === 'T'){
+        result += 'U'
+     }else{
+         result += dna[i]
+     }
+ }
+ return result
+  }
+
+  console.log(DNAtoRNA('GCAT'))//  => GCAU
+  console.log(DNAtoRNA('GATC'))//  => GAUC
+  console.log(DNAtoRNA('TAGC'))//  => UAGC
