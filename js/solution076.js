6ffe94a53d7af71707bf2c5244bbc44b686fbc29
Arreola <ra3020343@gmail.com>
2022-08-16 14:53:10 -0700

    solution077 completed

--- js/solution076.js
+++ js/solution076.js
@@ -8,3 +8,7 @@ console.log(findShort("bitcoin take over the world maybe who knows perhaps"))//3
 console.log(findShort("Waves ProofOfStake Bitcoin Ripple Monero Steem DarkCoin Dash Lisk LiteCoin Mine BTC Monero"))//3
 // take in a string of words
 //return the length of shortest word
+
+/*Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types. */
