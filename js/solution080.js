225a971a6cd5aa89452eaced27d86ee819d10147
Arreola <ra3020343@gmail.com>
2022-08-21 17:33:04 -0700

    solution080 completed

---
+++ js/solution080.js
@@ -0,0 +1,11 @@
+function sumTwoSmallestNumbers(numbers) {  
+    return numbers.sort((a,b)=>a-b).reduce((a,b)=>numbers[0]+numbers[1])
+  }
+  console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]))//7
+  console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]))//3453455
+
+  /*Create a function that returns the sum of the two lowest positive numbers given an array 
+  of minimum 4 positive integers. No floats or non-positive integers will be passed */
+  //take in an array of minimum 4 positive integers
+  //return the sum of the two lowest numbers
+  
