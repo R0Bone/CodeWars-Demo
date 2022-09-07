afde9e955dbbab49f492164c87ad4a645ebb5ddc
Arreola <ra3020343@gmail.com>
2022-09-06 22:31:43 -0700

    solution091 changes

--- js/solution091.js
+++ js/solution091.js
@@ -1,9 +1,21 @@
 /*Write a function that takes a single string (word) as argument. The function must return an ordered list 
 containing the indexes of all capital letters in the string.*/
 
+//const upper = str => str == str.toUpperCase()
+
 var capitals = function (word) {
-	// Write your code here
-};
+
+let indexxx = []
+for(let i = 0; i < word.length;i++){
+    if(word[i]== word[i].toUpperCase()){
+        indexxx.push(i)
+    }
+}
+	  return indexxx
+        
+    
+    }
+
 
 console.log(capitals('CodEWaRs')) //[0,3,4,6]
 
