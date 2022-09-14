2345c77f3e13883d13e4e3e290aaa22be54c436c
Arreola <ra3020343@gmail.com>
2022-09-14 15:50:14 -0700

    solution94 resubmitted

--- js/solution094.js
+++ js/solution094.js
@@ -8,7 +8,7 @@ Write a function that returns both the minimum and maximum number of the given l
 All arrays or lists will always have at least one element, so you don't need to check the length.
  Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
 */
-function minMax(arr){
+/* function minMax(arr){   #1 solution 
 arr = arr.sort((a,b)=> a-b)
     let x = arr[arr.length-1]
     let y = arr[0]
@@ -16,4 +16,13 @@ arr = arr.sort((a,b)=> a-b)
   }
 
   console.log(minMax([1,2,3,4,5])) //[1,5]
-  console.log(minMax([1]))         // [1,1]
+  console.log(minMax([1]))         // [1,1]  */
+
+  function minMax(arr){    //#2 solution 
+    //arr = arr.sort((a,b)=> a-b)
+        return [Math.min(...arr),Math.max(...arr)]
+      }
+
+      console.log(minMax([1,2,3,4,5])) //[1,5]
+      console.log(minMax([1]))         // [1,1]
+      console.log(minMax([6,5,2,77,23,24,9]))         // [2,77]
