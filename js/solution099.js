b486f862d5f4aee7583905dcfc083576f8d0fa6a
Arreola <ra3020343@gmail.com>
2022-09-25 14:53:58 -0700

    solution099 completed

---
+++ js/solution099.js
@@ -0,0 +1,8 @@
+function check(a, x) {
+    return a.includes(x)
+  }
+
+  console.log(check([66, 101], 66))//true
+
+  //take in an array (a) and a value (x). both can be either integers or strings
+  // return boolean 
