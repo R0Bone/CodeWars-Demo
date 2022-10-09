2df84eff57650aacf823eeb04f5e18ad30334bed
Arreola <ra3020343@gmail.com>
2022-10-08 19:18:22 -0700

    solution106 completed

--- js/solution106.js
+++ js/solution106.js
@@ -1,14 +1,22 @@
 function dontGiveMeFive(start, end){
+  
+    
     let total = 0
-    let result = 0
+    
     for(let i=start;i <= end;i++){
-        if(i % 5===0){
-            result += i
-        }else{
-            total +=1
+        let num = i.toString().split('') 
+        let checkIfFive = num.filter(number => number == '5').length
+            if(checkIfFive==0){
+                total += 1
+            }
+    
         }
+        return total
+        
     }
-  return total
-}
+  
+
 console.log(dontGiveMeFive(1,9))// 8
 console.log(dontGiveMeFive(4,17))// 12
+
+
