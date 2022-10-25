4e204258e1a8f33b447814397a9e917658894aa0
Arreola <ra3020343@gmail.com>
2022-10-24 20:55:43 -0700

    solution108 completed

---
+++ js/solution108.js
@@ -0,0 +1,18 @@
+/*Just a simple sorting usage. Create a function that returns the elements of 
+the input-array / list sorted in lexicographical order.*/
+
+sortme = function( names ){
+    return names.sort()
+   }
+   console.log(sortme(['one', 'two', 'three' ]))// ["one", "three", "two"])
+   //take in an array of strings, no integers, no funny business
+   //return an array in lexicographical order 
+
+   
+   
+   //function myLanguages(results) {
+    //return results.property
+
+//}
+
+//console.log(myLanguages({"Java": 10, "Ruby": 80, "Python": 65})) //["Ruby", "Python"]
